8f1555ded5ca763c12199218115ee45ac0cb0ee2
Cem Gencer <cem.gencer@me.com>
2017-11-15 22:09:57 +0300

    express running

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ var http = require('http');
 var crypto = require('crypto');
 var config = require( "./config.json" );
 var async = require( "async" );
+var parallel = require('run-parallel');
+var path = require('path');
+var _ = require('lodash');
+
+var express = require('express'),
+	expressJade = require('express-jade'),
+	serveStatic = require('serve-static'),
+	stylus = require('stylus'),
+	nib = require('nib');
 
 var CryptoJS = require("crypto-js");
-var gameSparks = new require('./node_modules/gamesparks-node/GameSparks.js');
+var gameSparks = new require('./vendor/gamesparks-node/GameSparks.js');
 
 var OKCupid = require('okcupidjs');
 var okc = new OKCupid();
@@ -16,51 +25,87 @@ function onMessage( msg ) {
 function onErr( err ) {
 	console.log( "-------------------------------------------------------------" );
 	console.log( "received an error:", err );
-}
-function handleReq( req, res ) {
-	var nonce = req.url.substring(1);
-	var processed = crypto.createHmac('SHA256', config.secret).update(nonce).digest('base64');
-	res.end(processed);
-}
-gameSparks.initPreviewListener( config.gameApiKey, config.secret, 10, onMessage, app, function( err ) {
-	console.log( "initializing preview listener:", err );
 	process.exit( 1 );
+}
+
+var app = express()
+app.set('views', __dirname + '/public/views/');
+app.set('view engine', 'jade');
+//app.use(express.logger('dev'));
+app.use(stylus.middleware({ 
+	src: __dirname + '/public/views/',
+	compile: function(str, path) {
+		return stylus(str).set( 'filename', path ).use( nib() );
+	}
+}));
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+	res.render('index',
+		{ title : 'Home' }
+	);
 });
+app.listen(3000);
+
+/*
+
+gameSparks.initPreviewListener( config.gameApiKey, config.secret, 10, onMessage, app, onErr);
+
 function app() {
 	console.log( "Gamesparks ready!" );
 
-okc.login(config.okc.username, config.okc.password, function(err, res, body) {
-	okc.getQuickmatch(function(err, res, body) {
-		okc.getUserProfile(body.sn, function(err, res, body) {
-			console.log( JSON.stringify( res, null, 2 ) );
-		});
-	});
-});
+//	rip25Members();
+}
 
+function rip25Members() {
+	okc.login(config.okc.username, config.okc.password, function(err, res, body) {
+		var fArr = [];
+			fArr.push(function (callback) {
+				setTimeout(function () {
 
-	async.waterfall([
-		function( cb ) {
-			// Authenticate
-			gameSparks.sendAs( null, ".AuthenticationRequest", {
+					okc.getQuickmatch(function(err, res, body) {
+
+						if(_.isUndefined(body.sn)) {
+							okc.getUserProfile(body.sn, function(err, res, body) {
+								if (!_.isUndefined(body.photos) && body.photos.length > 0) {
+
+									var ja = (!_.isUndefined(body.essays) && body.essays.length > 0) && (!_.isUndefined(body.essays[0].essay) && body.essays[0].essay.length > 0) ? body.photos[0].image_url : '';
+
+									var o = {
+										photo: _.isUndefined(body.photos) ? '' : body.photos[0].image_url,
+										location: body.location,
+										city: 'İstanbul',
+										country: 'Turkey',
+										gender: body.gender,
+										about: ja,
+										username: _.isUndefined(body.username) ? '' : body.username,
+										age: _.isUndefined(body.age) ? Math.floor(30+Math.random()*25) : body.age,
+										last_online: _.isUndefined(body.last_online) ? '' : body.last_online
+									};
+									console.log(o);
+									callback(null, o);
+								}
+							});
+						}
+					});
+
+				}, 100);
+			});
+
+		parallel(fArr, function (err, res) {
+			gameSparks.sendAs( null, ".RegistrationRequest", {
 				userName: config.testUser.username,
-				password: config.testUser.password
+				displayName: config.testUser.username,
+				password: '123',
+				segments: res
 			}, function( err, user ) {
 				if ( err )
 					return cb( err );
 				else 
 					return cb( null, user );
 			});
-		}, function( user, cb ) {
-			gameSparks.sendAs( user.userId, ".AccountDetailsRequest", {}, function( err, res ) {
-				if ( err ) return cb( err );
-				console.log( "account details:", JSON.stringify( res, null, 2 ) );
-				cb( null, user );
-			});
-		}
-	], function( err ) {
-		if ( err ) console.log( err );
-		// If you want to listen for messages back from the Gamesparks server, then do
-		// not exit() here.  Just sit around and wait ...
-		process.exit(0);
+		});
+
 	});
 }
+*/
